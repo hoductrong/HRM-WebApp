a79e9bb141a3bcc48ca7eda0f4c01f6a2c4e9462
trong.ho <trong.hoduc@gmail.com>
2017-12-19 19:20:51 +0700

    Tich hop template vao angular

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { TestClientComponent } from './test-client/test-client.component';
+import { AuthGuard } from './components/shared';
 
 const routes : Routes = [
-  {path : 'test',component: TestClientComponent}
+  { path: '', loadChildren: './components/layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
+  { path: 'login', loadChildren: './components/login/login.module#LoginModule' },
+  { path: 'signup', loadChildren: './components/signup/signup.module#SignupModule' },
+  { path: 'error', loadChildren: './components/server-error/server-error.module#ServerErrorModule' },
+  { path: 'access-denied', loadChildren: './components/access-denied/access-denied.module#AccessDeniedModule' },
+  { path: 'not-found', loadChildren: './components/not-found/not-found.module#NotFoundModule' },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
